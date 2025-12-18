54cd263d60e711b1d7493a447ab4050eb59f1ad1
Defor0127 <721snglwhs@naver.com>
2025-12-18 19:17:55 +0900

    sync temp

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -12,8 +12,7 @@ export default new DataSource({
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-
-  synchronize: false,
+  synchronize: true,
   logging: true,
 
   entities: [
